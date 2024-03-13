f363795e4685c8d21f2bd8d94017fed652c36616
yeohj0710 <yeohj0710@yonsei.ac.kr>
2024-03-13 14:45:42 +0900

    2403131445 suspense

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,26 +1,21 @@
-import { API_URL } from "../../../(home)/page";
-
-async function getMovie(id: string) {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  const response = await fetch(`${API_URL}/${id}`);
-  return response.json();
-}
-
-async function getVideos(id: string) {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
-}
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
 
 export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
   return (
     <div>
-      <h1>{movie.title}</h1>
+      <h2>Movie detail page</h2>
+      <Suspense fallback={<h3>Loading movie info...</h3>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h3>Loading movie videos...</h3>}>
+        <MovieVideos id={id} />
+      </Suspense>
     </div>
   );
 }
