03de19438da25b9cdba7af20b01057ef97c30770
yeohj0710 <yeohj0710@yonsei.ac.kr>
2024-03-13 17:33:01 +0900

    2403131732

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,9 @@
 import Link from "next/link";
+import { API_URL } from "../constants";
 
 export const metadata = {
   title: "Home",
 };
-
-export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
   await new Promise((resolve) => setTimeout(resolve, 1000));
--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,4 +1,4 @@
-import { API_URL } from "../app/(home)/page";
+import { API_URL } from "../app/constants";
 
 async function getMovie(id: string) {
   await new Promise((resolve) => setTimeout(resolve, 1000));
