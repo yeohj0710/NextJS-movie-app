b5a81b6b18207b7fd7446f7072da8dd676f5453b
yeohj0710 <yeohj0710@yonsei.ac.kr>
2024-03-13 14:03:07 +0900

    2403131403 ReactJS client side rendering example

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,21 @@
-export const metadata = {
-  title: "Home",
-};
+"use client";
 
-export default function Root() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+import { useState, useEffect } from "react";
+
+export default function Page() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    // client side rendering의 경우, fetch는 항상 client에서 일어난다.
+    const response = await fetch(
+      "https://nomad-movies.nomadcoders.workers.dev/movies"
+    );
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return <div>{isLoading ? "Loading..." : JSON.stringify(movies)}</div>;
 }
